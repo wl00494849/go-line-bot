9f875ec3d6699a3603a35cd2f685bc7193b08000
AKA迪迪弄 <wl00494849@gmail.com>
2022-03-26 17:22:11 +0800

    port

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	var port string
 	flag.StringVar(&port, "p", ":6666", "port")
 
-	if len(os.Getenv("Port")) > 0 {
+	if len(os.Getenv("PORT")) > 0 {
 		port = os.Getenv("Port")
 	}
 
